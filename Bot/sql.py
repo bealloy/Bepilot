3103bcbf27ae945e29166f52d93c0eb4bd0a754c
Shivani-Sharma-23 <shivanisharma232005@gmail.com>
2024-04-30 22:49:51 +0530

    Done

--- Bot/sql.py
+++ Bot/sql.py
@@ -1,2 +1,21 @@
-import pyodbc 
-cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=myserver;Database=SQl_taipy;Port=myport;User ID=Shiv456;Password=12345')
+import sqlite3
+
+conn = sqlite3.connect('Taipy.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT,
+                    email TEXT)''')
+
+cursor.execute("INSERT INTO users (username, email) VALUES (?, ?)", ('JohnDoe', 'john@example.com'))
+
+cursor.execute("SELECT * FROM users")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.commit()
+conn.close()
